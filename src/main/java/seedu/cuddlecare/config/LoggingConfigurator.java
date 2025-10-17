43a83abac01b1473aa67be03a32d240ae38df574
Harshit Srivastava <harshitsrivastava7149@gmail.com>
2025-10-17 20:58:40 +0800

    Create a config class for the root logger

---
+++ src/main/java/seedu/cuddlecare/config/LoggingConfigurator.java
@@ -0,0 +1,43 @@
+package seedu.cuddlecare.config;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.logging.FileHandler;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+public class LoggingConfigurator {
+
+    private static final Path LOG_DIR_PATH = Paths.get("logs");
+    private static final String LOG_FILE_PATH = "logs/cuddlecare_app.log";
+
+    public static void setup() {
+        Logger rootLogger = Logger.getLogger("");
+
+        for (Handler handler: rootLogger.getHandlers()) {
+            rootLogger.removeHandler(handler);
+        }
+
+        rootLogger.setLevel(Level.INFO);
+
+        try {
+
+            if (Files.notExists(LOG_DIR_PATH)) {
+                Files.createDirectories(LOG_DIR_PATH);
+                System.out.println("INFO: Created logging directory: " + LOG_DIR_PATH.toAbsolutePath());
+            }
+
+            FileHandler fileHandler = new FileHandler(LOG_FILE_PATH, 0, 1, true);
+            fileHandler.setFormatter(new SimpleFormatter());
+            rootLogger.addHandler(fileHandler);
+
+        } catch (IOException e) {
+            System.err.println("FATAL: Could not set up logging");
+            e.printStackTrace();
+        }
+    }
+}
