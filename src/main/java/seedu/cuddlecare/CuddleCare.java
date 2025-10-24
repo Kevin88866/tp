8449bba305f27b0c76efcf9cd422c55438cbea33
Harshit Srivastava <harshitsrivastava7149@gmail.com>
2025-10-24 23:21:16 +0800

    Implement OverdueTreatmentCommand
    OverdueTreatmentCommand takes in an optional petName and prints all the overdue treatments for that pet (or all pet if no name was provided)

--- src/main/java/seedu/cuddlecare/CuddleCare.java
+++ src/main/java/seedu/cuddlecare/CuddleCare.java
@@ -1,21 +1,9 @@
 package seedu.cuddlecare;
 
 import seedu.cuddlecare.command.Command;
-import seedu.cuddlecare.command.impl.AddPetCommand;
-import seedu.cuddlecare.command.impl.AddTreatmentCommand;
-import seedu.cuddlecare.command.impl.ByeCommand;
-import seedu.cuddlecare.command.impl.DeletePetCommand;
-import seedu.cuddlecare.command.impl.DeleteTreatmentCommand;
-import seedu.cuddlecare.command.impl.EditPetCommand;
-import seedu.cuddlecare.command.impl.FindCommand;
-import seedu.cuddlecare.command.impl.ListAllTreatmentsCommand;
-import seedu.cuddlecare.command.impl.ListPetTreatmentsCommand;
-import seedu.cuddlecare.command.impl.ListPetsCommand;
-import seedu.cuddlecare.command.impl.MarkTreatmentCommand;
-import seedu.cuddlecare.command.impl.UnmarkTreatmentCommand;
+import seedu.cuddlecare.command.impl.*;
 import seedu.cuddlecare.config.LoggingConfigurator;
 import seedu.cuddlecare.parser.Parser;
-import seedu.cuddlecare.command.impl.FilterTreatmentByDateCommand;
 
 import java.util.Map;
 import java.util.Scanner;
@@ -136,7 +124,8 @@ public class CuddleCare {
                 Map.entry("delete-treatment", new DeleteTreatmentCommand(pets)),
                 Map.entry("find", new FindCommand(pets)),
                 Map.entry("edit-pet", new EditPetCommand(pets)),
-                Map.entry("treatment-date", new FilterTreatmentByDateCommand(pets))
+                Map.entry("treatment-date", new FilterTreatmentByDateCommand(pets)),
+                Map.entry("overdue-treatments", new OverdueTreatmentsCommand(pets))
         );
         assert commands != null : "Commands map cannot be null";
         parser.setCommands(commands);
---
+++ src/main/java/seedu/cuddlecare/command/impl/OverdueTreatmentsCommand.java
@@ -0,0 +1,119 @@
+package seedu.cuddlecare.command.impl;
+
+import seedu.cuddlecare.Pet;
+import seedu.cuddlecare.PetList;
+import seedu.cuddlecare.Treatment;
+import seedu.cuddlecare.command.Command;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class OverdueTreatmentsCommand implements Command {
+
+    private static final Logger LOGGER = Logger.getLogger(OverdueTreatmentsCommand.class.getName());
+
+    private static final String SYNTAX = "overdue-treatments [n/PET_NAME]";
+
+    private final PetList pets;
+
+    public OverdueTreatmentsCommand(PetList pets) {
+        assert pets != null : "Pets cannot be null";
+        this.pets = pets;
+    }
+
+    public void exec(String args) {
+        assert args != null : "args cannot be null";
+
+        LOGGER.log(Level.INFO, "Executing Overdue treatment command with args: " + args);
+
+        if (pets.size() == 0) {
+            System.out.println("No pets added");
+            LOGGER.log(Level.INFO, "No pets added yet.");
+            return;
+        }
+
+        Pet pet = getPetByName(args);
+
+        if (!args.isEmpty() && pet == null) {
+            return;
+        }
+
+        Stream<Pet> targetPet = getPetStream(pet);
+
+        LocalDate presentDate = LocalDate.now();
+        Map<Pet, ArrayList<Treatment>> overdueTreatments = getOverdueTreatments(targetPet, presentDate);
+
+        printOverdueTreatments(overdueTreatments, pet, presentDate);
+    }
+
+    private Map<Pet, ArrayList<Treatment>> getOverdueTreatments(Stream<Pet> targetPet, LocalDate presentDate) {
+        assert targetPet != null : "Target pet cannot be null. It's either a stream of all pets or just the input pet.";
+        assert presentDate != null : "Present date cannot be null";
+        return targetPet
+                .filter(p -> p.getTreatments().stream()
+                        .anyMatch(t -> !t.isCompleted() && t.getDate().isBefore(presentDate)))
+                .collect(Collectors.toMap(
+                        p -> p,
+                        p -> p.getTreatments().stream()
+                                .filter(t -> !t.isCompleted() && t.getDate().isBefore(presentDate))
+                                .collect(Collectors.toCollection(ArrayList::new))
+                ));
+    }
+
+    private Stream<Pet> getPetStream(Pet pet) {
+        return pet == null ? pets.stream() : Stream.of(pet);
+    }
+
+    private Pet getPetByName(String args) {
+        String[] tags = args.split(" (?=[\\w+]/)");
+        String petName = null;
+        for (String tag : tags) {
+            if (tag.startsWith("n/")) {
+                petName = tag.substring(2).trim();
+                break;
+            }
+        }
+        Pet pet = pets.getPetByName(petName);
+
+        if (petName == null) {
+            LOGGER.log(Level.INFO, "Invalid args provided");
+            System.out.printf("Invalid arguments provided.%nSyntax: %s", SYNTAX);
+        } else if (pet == null) {
+            LOGGER.log(Level.INFO, "No pet found");
+            System.out.printf("No pet found with the name: %s%n", petName);
+        }
+
+        return pet;
+    }
+
+    private void printOverdueTreatments(Map<Pet, ArrayList<Treatment>> treatments, Pet inputPet, LocalDate presentDate) {
+        assert presentDate != null : "Present date cannot be null";
+        assert treatments != null : "Treatments cannot be null";
+        if (treatments.isEmpty()) {
+            LOGGER.log(Level.INFO, "No overdue Treatment");
+            System.out.printf("No overdue treatment%s. Way to go!%n", (inputPet == null ? "" : " for " + inputPet.getName()));
+            return;
+        }
+        System.out.printf("Overdue Treatments%s:%n", (inputPet == null ? "" : " for " + inputPet.getName()));
+        for (Map.Entry<Pet, ArrayList<Treatment>> entry: treatments.entrySet()) {
+
+            for (Treatment treatment: entry.getValue()) {
+
+                long overdueSince = ChronoUnit.DAYS.between(treatment.getDate(), presentDate);
+
+                if (inputPet == null) {
+                    System.out.printf("%s: \"%s\" was due on %s (overdue for %d days)%n", entry.getKey().getName(), treatment.getName(), treatment.getDate(), overdueSince);
+                } else {
+                    System.out.printf("\"%s\" was due on %s (overdue for %d days)%n", treatment.getName(), treatment.getDate(), overdueSince);
+                }
+            }
+        }
+        LOGGER.log(Level.INFO, "Successfully executed the command");
+    }
+}
