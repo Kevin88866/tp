90515432d70b384c1e8ee81c9683b96abc78793b
Zheng_Kaiwen <zkw2542248162@gmail.com>
2025-10-28 11:49:43 +0800

    extract the parser part of the command to the parser folder to increase code quality

---
+++ src/main/java/seedu/cuddlecare/parser/args/EditPetArgs.java
@@ -0,0 +1,10 @@
+package seedu.cuddlecare.parser.args;
+
+/** Args for edit-pet. */
+public final class EditPetArgs {
+    public String oldName;
+    public String newName;
+    public String species;
+    public Integer age;
+    public boolean valid;
+}
---
+++ src/main/java/seedu/cuddlecare/parser/args/UnmarkTreatmentArgs.java
@@ -0,0 +1,8 @@
+package seedu.cuddlecare.parser.args;
+
+/** Args for unmark (n/PET_NAME i/INDEX). */
+public final class UnmarkTreatmentArgs {
+    public String petName;
+    public int index;
+    public boolean valid;
+}
