7e40af8af53cbf7a7b215cac243fe0ba640c70ca
Harshit Srivastava <harshitsrivastava7149@gmail.com>
2025-10-29 02:52:01 +0800

    Update Help Command
    The help command now works properly. It displays all commands if no arguments are provided, and displays help specific to a command if a correct commandname is provided

--- src/main/java/seedu/cuddlecare/command/impl/HelpCommand.java
+++ src/main/java/seedu/cuddlecare/command/impl/HelpCommand.java
@@ -10,6 +10,8 @@ import java.util.stream.Stream;
 
 public class HelpCommand implements Command {
 
+    private static final String SYNTAX = "help [c/COMMAND_NAME]";
+
     private Map<String, Command> commandsMap;
     
     public void setCommands(Map<String, Command> commands) {
@@ -19,7 +21,36 @@ public class HelpCommand implements Command {
 
 
     public void exec(String args) {
-        printAllCommands();
+        String commandName = getCommandName(args);
+        if (!args.isEmpty() && commandName == null) {
+            System.out.printf("Invalid Syntax.\n%s", SYNTAX);
+            return;
+        }
+
+        if (commandName != null && !commandsMap.containsKey(commandName.toLowerCase())) {
+            System.out.printf("Command \"%s\" not found. Run \"help\" for a list of all available commands.%n", commandName);
+            return;
+        }
+
+        if (commandName!=null) {
+            printCommand(commandName, commandsMap.get(commandName.toLowerCase()));
+        } else {
+            printAllCommands();
+        }
+    }
+
+    private String getCommandName(String args) {
+        return getTagValue(args, "c");
+    }
+
+    private String getTagValue(String args, String tag) {
+        String [] parts = args.split("  (?=[\\w+]/)");
+        for (String part: parts) {
+            if (part.startsWith(tag)) {
+                return part.substring(tag.length()+1);
+            }
+        }
+        return null;
     }
 
     private void printAllCommands() {
@@ -35,12 +66,12 @@ public class HelpCommand implements Command {
     private void printCommandsByCategory(String category, Map<String, Command> commandsOfCategory) {
         System.out.println(category);
         for (Map.Entry<String, Command> command: commandsOfCategory.entrySet()) {
-            printCommand(command.getKey(), command.getValue());
+            System.out.printf("\t%s: %s%n", command.getKey(), command.getValue().getShortDescription());
         }
     }
 
     private void printCommand(String commandName, Command command) {
-        System.out.printf("\t%s: %s%n", commandName, command.getShortDescription());
+        System.out.printf("\tCommand Name: %s%n\tCategory: %s%n\tDescription: %s%n\tSyntax: %s%n%n\t*[t/tag] means tag is an optional argument.%n", commandName, String.join(",", command.getCategory()) ,command.getLongDescription(), command.getSyntax());
     }
 
     private Map<String, Map<String, Command>> getCategorizedCommands() {
