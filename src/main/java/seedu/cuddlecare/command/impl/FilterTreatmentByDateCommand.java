8a1ca03bea2331302819737badf9603d2f5c4044
jieling <jieling834@gmail.com>
2025-10-29 14:00:44 +0800

    Fix unit test errors
    Unit tests were failing due to changes in the way the list is printed,
    hence unit tests were updated to reflect the changes.

--- src/main/java/seedu/cuddlecare/command/impl/FilterTreatmentByDateCommand.java
+++ src/main/java/seedu/cuddlecare/command/impl/FilterTreatmentByDateCommand.java
@@ -14,6 +14,7 @@ import java.util.logging.Logger;
 import java.util.logging.Level;
 
 //@@author Pavithra6-Srinivasan
+
 /**
  * A command that filters treatments by date range across all pets.
  * <p>
@@ -74,7 +75,8 @@ public class FilterTreatmentByDateCommand implements Command {
                 return;
             }
 
-            Ui.printHeader("Treatment Summary from: " + fromDate + " to: " + toDate);
+            Ui.printHeader("Found " + filteredTreatments.size() +
+                    " treatment(s) from " + fromDate + " to " + toDate + ":");
             Ui.printList(filteredTreatments);
 
         } catch (Exception e) {
